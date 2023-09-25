f3a91dbbe7730576b997e29c94341c54933643b5
Данил Кочетов <dmi194@yandex.ru>
2023-09-26 00:22:10 +0300

    created applications users, api, sales_forecasts

--- backend/forecast/settings.py
+++ backend/forecast/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'sales_forecasts.apps.SalesForecastsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
