562b138d1cff19d730e9c3c0de9ffdf1e2b82b0d
Pavel Lashkov <hutjicsgo@gmail.com>
2023-09-28 22:44:45 +0300

    new version

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from sales_forecasts.models import User
+from django.utils import timezone
+
+from categories.models import Category, Group, Subcategory
+from sales.models import Sales, SalesRecord
+from stores.models import Stores, Store
+from sales_forecasts.models import Forecast, ForecastSku, ForecastDaily
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,3 +21,101 @@ class UserSerializer(serializers.ModelSerializer):
         """Создает нового пользователя."""
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Category'''
+    class Meta:
+        fields = ('sku', 'group', 'category', 'subcategory', 'uom')
+        model = Category
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Group'''
+    class Meta:
+        fields = ('id', 'group')
+        model = Group
+
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Subcategory'''
+    class Meta:
+        fields = ('subcategory', 'category')
+        model = Subcategory
+
+
+class SalesSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Sales'''
+    store = serializers.PrimaryKeyRelatedField(
+        queryset=Stores.objects.all()
+    )
+    sku = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        fields = ('store', 'sku')
+        model = Sales
+
+
+class SalesRecordSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели SalesRecord'''
+    class Meta:
+        fields = ('fact', 'date', 'sales_type', 'sales_units', 'sales_units_promo',
+                  'sales_rub', 'sales_rub_promo')
+        model = SalesRecord
+
+
+class StoreSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели Store'''
+    class Meta:
+        fields = ('store_name', 'city', 'division',
+                  'type_format', 'loc', 'size',
+                  'is_active')
+        model = Store
+
+
+class ForecastSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели прогноза данных'''
+    class Meta:
+        model = Forecast
+        fields = ['id', 'store', 'forecast_date']
+
+    def validate(self, data):
+        if data['forecast_date'] < timezone.now().date():
+            raise serializers.ValidationError('Дата прогноза не может быть в прошлом')
+        return data
+
+    def create(self, validated_data):
+        forecast = Forecast.objects.create(**validated_data)
+        return forecast
+
+    def update(self, instance, validated_data):
+        instance.store = validated_data.get('store', instance.store)
+        instance.forecast_date = validated_data.get('forecast_date', instance.forecast_date)
+        instance.save()
+        return instance
+    
+
+class ForecastSkuSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели прогноза товара'''
+    class Meta:
+        model = ForecastSku
+        fields = ['id', 'forecast', 'sku']
+
+    def create(self, validated_data):
+        forecast_sku = ForecastSku.objects.create(**validated_data)
+        return forecast_sku
+
+    def update(self, instance, validated_data):
+        instance.forecast = validated_data.get('forecast', instance.forecast)
+        instance.sku = validated_data.get('sku', instance.sku)
+        instance.save()
+        return instance
+
+
+class ForecastDailySerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели ежедневного прогноза'''
+    class Meta:
+        model = ForecastDaily
+        fields = '__all__'
---
+++ backend/categories/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+
+
+class Group(models.Model):
+    """Модель групы товаров."""
+    title = models.CharField(
+        max_length=75, verbose_name='Название группы', unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """Модель категории товаров."""
+    title = models.CharField(
+        max_length=75, verbose_name='Название категории', unique=True
+    )
+    group = models.ForeignKey(
+        Group,
+        verbose_name='Группа',
+        on_delete=models.CASCADE,
+        related_name='categories',
+        to_field='title',
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Subcategory(models.Model):
+    """Модель подкатегории товаров."""
+    title = models.CharField(
+        max_length=75, verbose_name='Название подкатегории', unique=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.CASCADE,
+        related_name='subcategories',
+        to_field='title',
+    )
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    """Модель продуктов."""
+    sku = models.CharField(
+        max_length=75, verbose_name='Название продукта', unique=True
+    )
+    subcategory = models.ForeignKey(
+        Subcategory,
+        verbose_name='Подкатегория',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='products',
+        to_field='title',
+    )
+    uom = models.SmallIntegerField(
+        verbose_name='Маркер единицы измерения',
+        default=1,
+    )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['sku']
+
+    def __str__(self):
+        return self.sku
