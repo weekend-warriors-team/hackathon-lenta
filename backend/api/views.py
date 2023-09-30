5ed0c96b0ca0ed5da410734506a6cbd2abd3d114
Данил Кочетов <dmi194@yandex.ru>
2023-09-30 21:53:25 +0300

    Добавил вью и сериализатор для продуктов

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
+from categories.models import Category, Group, Product, Subcategory
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework import viewsets
 from stores.models import Store
 from users.models import User
 
-# from .filters import StoreFilter
-from .serializers import StoreSerializer, UserSerializer
+from .serializers import ProductSerializer, StoreSerializer, UserSerializer
 
 
 class UserViewSet(DjoserUserViewSet):
@@ -14,7 +14,7 @@ class UserViewSet(DjoserUserViewSet):
     serializer_class = UserSerializer
 
 
-class ShopsViewSet(viewsets.ModelViewSet):
+class ShopViewSet(viewsets.ModelViewSet):
     """Вьюсет для работы с магазинами."""
     http_method_names = ['get']
     queryset = Store.objects.all()
@@ -22,4 +22,15 @@ class ShopsViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = [
         'store', 'city', 'division', 'type_format', 'loc', 'size', 'is_active'
+    ]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """Вьюсет для работы с продуктами."""
+    http_method_names = ['get']
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        'subcategory', 'uom'
     ]
