4f84907555858beff15ed1698df7365637531ab2
Данил Кочетов <dmi194@yandex.ru>
2023-09-30 19:00:31 +0300

    Удалил закоменченый код в модели

--- backend/stores/models.py
+++ backend/stores/models.py
@@ -27,46 +27,6 @@ class Division(models.Model):
         return self.division
 
 
-# class Type_format(models.Model):
-#     '''Класс магазинов.'''
-#     type_format = models.SmallIntegerField(unique=True)
-# 
-#     class Meta:
-#         verbose_name = 'Формат магазина'
-#         verbose_name_plural = 'Форматы магазинов'
-#         ordering = ('type_format',)
-# 
-#     def __str__(self):
-#         return self.type_format
-# 
-# 
-# class Location(models.Model):
-#     '''Класс локации.'''
-#     loc = models.SmallIntegerField(unique=True)
-# 
-#     class Meta:
-#         verbose_name = 'Локация магазина'
-#         verbose_name_plural = 'Локации магазинов'
-#         ordering = ('loc',)
-# 
-#     def __str__(self):
-#         return self.loc
-# 
-# 
-# class Size(models.Model):
-#     """Класс размеров магазинов."""
-# 
-#     size = models.SmallIntegerField(unique=True, null=True)
-# 
-#     class Meta:
-#         verbose_name = 'Размер'
-#         verbose_name_plural = 'Размеры'
-#         ordering = ('size',)
-# 
-#     def __str__(self):
-#         return str(self.size)
-
-
 class Store(models.Model):
     """Класс таблицы магазинов продуктовой иерархии."""
 
