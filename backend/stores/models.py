a986a24955d454469a1251b0b14dfd63c6c5a3fb
Pavel Lashkov <hutjicsgo@gmail.com>
2023-09-30 13:48:56 +0300

    update

--- backend/stores/models.py
+++ backend/stores/models.py
@@ -1,70 +1,57 @@
 from django.db import models
-
-
-class Stores(models.Model):
-    '''Класс магазинов'''
-    store = models.CharField(max_length=50, unique=True)
-
-    class Meta:
-        verbose_name = 'Магазин',
-        verbose_name_plural = 'Магазины',
-        ordering = ('store',)
-
-    def __str__(self):
-        return self.store
-
+    
 
 class City(models.Model):
     '''Класс городов'''
     city = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = 'Город'
-        verbose_name_plural = 'Города'
-        ordering = ('city',)
+        verbose_name='Город'
+        verbose_name_plural='Города'
+        ordering=('city',)
 
     def __str__(self):
         return self.city
-
+    
 
 class Division(models.Model):
     '''Класс отделов магазинов'''
     division = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = 'Отдел'
-        verbose_name_plural = 'Отделы'
-        ordering = ('division',)
+        verbose_name='Отдел'
+        verbose_name_plural='Отделы'
+        ordering=('division',)
 
     def __str__(self):
         return self.division
-
+    
 
 class Type_format(models.Model):
     '''Класс магазинов'''
     type_format = models.IntegerField()
 
     class Meta:
-        verbose_name = 'Формат магазина'
-        verbose_name_plural = 'Форматы магазинов'
-        ordering = ('type_format',)
+        verbose_name='Формат магазина'
+        verbose_name_plural='Форматы магазинов'
+        ordering=('type_format',)
 
     def __str__(self):
         return self.type_format
-
+    
 
 class Location(models.Model):
     '''Класс локации'''
     loc = models.IntegerField()
 
     class Meta:
-        verbose_name = 'Локация магазина'
-        verbose_name_plural = 'Локации магазинов'
-        ordering = ('loc',)
+        verbose_name='Локация магазина'
+        verbose_name_plural='Локации магазинов'
+        ordering=('loc',)
 
     def __str__(self):
         return self.loc
-
+    
 
 class Size(models.Model):
     """Класс размеров магазинов."""
@@ -72,22 +59,17 @@ class Size(models.Model):
     size = models.IntegerField(null=True)
 
     class Meta:
-        verbose_name = 'Размер'
-        verbose_name_plural = 'Размеры'
-        ordering = ('size',)
+        verbose_name='Размер'
+        verbose_name_plural='Размеры'
+        ordering=('size',)
 
     def __str__(self):
         return str(self.size)
 
 
-class Store(models.Model):
+class Stores(models.Model):
     """Класс таблицы магазинов продуктовой иерархии."""
-
-    store = models.ForeignKey(
-        Stores,
-        on_delete=models.CASCADE,
-        related_name='stores',
-    )
+    store = models.CharField(max_length=50, unique=True)
     city = models.ForeignKey(
         City,
         on_delete=models.CASCADE
@@ -111,9 +93,9 @@ class Store(models.Model):
     is_active = models.BooleanField()
 
     class Meta:
-        verbose_name = 'Таблица магазина'
-        verbose_name_plural = 'Таблица магазинов'
-        ordering = ('store',)
+        verbose_name='Таблица магазина'
+        verbose_name_plural='Таблица магазинов'
+        ordering=('store',)
 
     def __str__(self):
         return str(self.store)
