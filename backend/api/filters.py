c0dcb79ba1b1b49b03b3b776bebae3b1d23eee27
Pavel Lashkov <hutjicsgo@gmail.com>
2023-10-01 21:22:26 +0300

    some fixes

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,9 +1,15 @@
 from django_filters.rest_framework import FilterSet, filters
-from sales.models import Sales
+from sales.models import Sale
 from stores.models import Store
 
 
-class StoresFilter(FilterSet):
+class StoresFilter(filters.FilterSet):
     '''Фильтр магазинов.'''
     model = Store
     fields = ('store', 'city', 'division', 'type_format', 'loc',)
+
+
+class SalesFilter(filters.FilterSet):
+    '''Фильтр продаж'''
+    model = Sale
+    fields = ('store', 'date', 'sales_type', 'sales_units', 'sales_units_promo', 'sales_rub', 'sales_rub_promo')
--- backend/stores/admin.py
+++ backend/stores/admin.py
@@ -4,16 +4,19 @@ from .models import City, Division, Store
 
 
 class CityAdmin(admin.ModelAdmin):
+    '''Админка городов'''
     list_display = ('city',)
     search_fields = ('city',)
 
 
 class DivisionAdmin(admin.ModelAdmin):
+    '''Админка дивизионов'''
     list_display = ('division',)
     search_fields = ('division',)
 
 
 class StoreAdmin(admin.ModelAdmin):
+    '''Админка магазинов'''
     list_display = (
         'store', 'city', 'division', 'type_format', 'loc', 'size', 'is_active'
     )
