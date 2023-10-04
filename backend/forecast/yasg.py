57bfc9374316680f7430a36943e98f1c312ca1a2
Данил Кочетов <dmi194@yandex.ru>
2023-10-04 21:31:16 +0300

    Добавил cors

--- backend/forecast/yasg.py
+++ backend/forecast/yasg.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -10,8 +10,8 @@ schema_view = get_schema_view(
       description="API documentation",
       license=openapi.License(name='BSD License'),
     ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
